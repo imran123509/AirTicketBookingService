da6bcf595e838b3599e26d64994d10633a712f7c
imran123509 <hasanimran114@gmail.com>
2024-01-12 15:25:10 +0530

    first commit with basic setup

--- src/index.js
+++ src/index.js
@@ -3,8 +3,15 @@ const bodyParser=require('body-parser')
 const app=express();
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({extended:true}))
-const {PORT}=require('./config/serverConfig')
- 
+const {PORT}=require('./config/serverConfig');
+
+const v1ApiRoutes=require('./routes/index')
+ const db=require('./models/index')
+app.use('/api', v1ApiRoutes);
 app.listen(PORT, ()=>{
     console.log(`server is connected ${PORT}`);
+    if(process.env.DB_SYNC){
+        // db.Sequelize.sync(alert:true)
+    }
+      
 })
